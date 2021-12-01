141898328ee9f02d899b56f6bf58f70101911d73
gokel66 <okellogeorge40@yahoo.com>
2021-11-30 19:50:36 -0600

    Add packer configuration with basic setup.

---
+++ lua/cvim/plugins.lua
@@ -0,0 +1,15 @@
+local execute = vim.api.nvim_command
+local fn = vim.fn
+
+local install_path = fn.stdpath('data')..'/site/pack/packer/opt/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+    execute('!git clone https//github.com/wbthomason/packer.nvim'..install_path)
+    execute 'packadd packer.nvim'
+end
+
+return require('packer').startup(function()
+    use { 'wbthomason/packer.nvim', opt = true }
+    use 'kyazdani42/nvim-tree.lua'
+    use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}
+end)
